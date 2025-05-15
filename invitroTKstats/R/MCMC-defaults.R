306e37564683b5d1fa621a6f258cf79dba989ed6
Sarah Davidson-Fritz <davidsonfritz.sarah@epa.gov>
2025-05-15 09:14:53 -0400

    delete the invitroTKstats tarball files and invitroTKstats R package sub-dir since they are not necessary for the data processing code repo; move the RData files kreutz2020, smeltz2020, and Crizer Clint 2024 data to final_RData sub-dir

--- invitroTKstats/R/MCMC-defaults.R
+++
@@ -1,8 +0,0 @@
-# The model file that is used by JAGS:
-JAGS.PATH <- "C:/Program Files/JAGS/JAGS-4.3.0/x64/bin/"
-RJAGS.MODEL.FILE <- "Z:/Research Projects/CeetoxHumanHTTK/PPB-JAGS-Analyses/PPB-model-affinity-101318.jags"
-
-# The number of Markov Chains used (should be >2 by hopefully <= number of CPU cores):
-NUM.CHAINS <- 5
-NUM.CORES <- 12
-RANDOM.SEED <- 1111
